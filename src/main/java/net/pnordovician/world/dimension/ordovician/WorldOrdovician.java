a00707b13f0bbeab02d8edabbce9c3eda58f60fe
AechtRob <aecht_rob@outlook.com>
2024-01-17 14:08:32 +0000

    Portals

--- src/main/java/net/pnordovician/world/dimension/ordovician/WorldOrdovician.java
+++ src/main/java/net/pnordovician/world/dimension/ordovician/WorldOrdovician.java
@@ -1021,7 +1021,7 @@ public class WorldOrdovician extends ElementsPNOrdovicianMod.ModElement {
 			entityIn.setWorld(toWorldIn);
 		}
 
-		private TeleporterDimensionMod getTeleporterForDimension(Entity entity, BlockPos pos, int dimid) {
+		public static TeleporterDimensionMod getTeleporterForDimension(Entity entity, BlockPos pos, int dimid) {
 			BlockPattern.PatternHelper bph = portal.createPatternHelper(entity.world, new BlockPos(pos));
 			double d0 = bph.getForwards().getAxis() == EnumFacing.Axis.X
 					? (double) bph.getFrontTopLeft().getZ()
